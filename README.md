1f63d5e0b165cc24a1a36b8f5a1f77caba696e81
ikerUrle <ikerurlezaga@gmail.com>
2020-11-29 13:49:32 +0100

    v0.1 Tirada Fate

---
+++ README.md
@@ -0,0 +1,7 @@
+## DadoBot! - Discord bot
+
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)
+
+### Bot de Discord creado para partidas de rol del juego Fate Acelerado.
+
+> ### Permite distintos comando para realizar tiradas **Fate**, **d20**, **d100**...
--- index.js
+++ index.js
@@ -8,10 +8,23 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+var rollMsgs = { "-4": "R.I.P", 0: "mediocre...", 4: "YEAH BABY" };
+const tiradaFate = () => {
+  var rollResult = Array.from({ length: 4 }, () =>
+    Math.floor(Math.random() * 3 - 1)
+  ).reduce((total, num) => total + num);
+
+  return `${rollResult} ${rollMsgs[rollResult] || ""}`;
+};
+
 client.on("message", (msg) => {
-  console.log(msg);
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
+  if (msg.content == "tirada" || msg.content == "!") {
+    msg.delete();
+    msg.channel.send(`${msg.author}: ${tiradaFate()}`);
+  } else if (msg.content == "clear") {
+    msg.channel.messages.fetch({ limit: 100 }).then((messages) => {
+      msg.channel.bulkDelete(messages);
+    });
   }
 });
 
