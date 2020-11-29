42cf3e3c3dc72e370a1297d706f9ed25a861d89f
ikerUrle <ikerurlezaga@gmail.com>
2020-11-29 16:07:12 +0100

    v0.2 - dX and Fate

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const dotenv = require("dotenv");
+const { env } = require("process");
 
 dotenv.config();
 
@@ -8,19 +9,36 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-var rollMsgs = { "-4": "R.I.P", 0: "mediocre...", 4: "YEAH BABY" };
 const tiradaFate = () => {
   var rollResult = Array.from({ length: 4 }, () =>
     Math.floor(Math.random() * 3 - 1)
   ).reduce((total, num) => total + num);
 
-  return `${rollResult} ${rollMsgs[rollResult] || ""}`;
+  return `*Tirada Fate*: **${rollResult}**`;
+};
+
+const tiradaDado = (num, dado) => {
+  var results = Array();
+  for (var i = 0; i < (num != "" ? num : 1); i++) {
+    results.push(Math.floor(Math.random() * dado + 1));
+  }
+
+  return `*Tirada ${num}d${dado}*: ${results.join(" + ")} **${
+    num != "" ? "= " + results.reduce((acc, value) => (acc += value)) : ""
+  }** `;
+};
+
+var enviarTirada = (msg, tirada) => {
+  msg.delete();
+  msg.channel.send(`*${msg.author}*: ${tirada}`);
 };
 
 client.on("message", (msg) => {
   if (msg.content == "tirada" || msg.content == "!") {
-    msg.delete();
-    msg.channel.send(`${msg.author}: ${tiradaFate()}`);
+    enviarTirada(msg, tiradaFate());
+  } else if (msg.content.match("![0-9]{0,1}d[0-9]*")) {
+    var values = msg.content.substring(1).split("d");
+    enviarTirada(msg, tiradaDado(values[0], values[1]));
   } else if (msg.content == "clear") {
     msg.channel.messages.fetch({ limit: 100 }).then((messages) => {
       msg.channel.bulkDelete(messages);
